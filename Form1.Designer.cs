d8eaa49531bfce3607969ba9eeb82adb43ec5dec
Moshe Oziel <uzikiko@gmail.com>
2015-09-22 08:45:29 +0300

    Init project

---
+++ Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace FieldFox_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Start_Freq_textBox = new System.Windows.Forms.TextBox();
+            this.End_Freq_textBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Points_num_textBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.IF_bandW_textBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Send_param_button = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Log_textBox = new System.Windows.Forms.TextBox();
+            this.Avg_num_textBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Point_radioButton = new System.Windows.Forms.RadioButton();
+            this.Sweep_radioButton = new System.Windows.Forms.RadioButton();
+            this.Folder_name_textBox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Measure_button = new System.Windows.Forms.Button();
+            this.Meas_time_textBox = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Start Freq";
+            // 
+            // Start_Freq_textBox
+            // 
+            this.Start_Freq_textBox.Location = new System.Drawing.Point(96, 28);
+            this.Start_Freq_textBox.Name = "Start_Freq_textBox";
+            this.Start_Freq_textBox.Size = new System.Drawing.Size(99, 20);
+            this.Start_Freq_textBox.TabIndex = 1;
+            // 
+            // End_Freq_textBox
+            // 
+            this.End_Freq_textBox.Location = new System.Drawing.Point(96, 63);
+            this.End_Freq_textBox.Name = "End_Freq_textBox";
+            this.End_Freq_textBox.Size = new System.Drawing.Size(99, 20);
+            this.End_Freq_textBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "End Freq";
+            // 
+            // Points_num_textBox
+            // 
+            this.Points_num_textBox.Location = new System.Drawing.Point(96, 99);
+            this.Points_num_textBox.Name = "Points_num_textBox";
+            this.Points_num_textBox.Size = new System.Drawing.Size(99, 20);
+            this.Points_num_textBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Points Num";
+            // 
+            // IF_bandW_textBox
+            // 
+            this.IF_bandW_textBox.Location = new System.Drawing.Point(96, 134);
+            this.IF_bandW_textBox.Name = "IF_bandW_textBox";
+            this.IF_bandW_textBox.Size = new System.Drawing.Size(99, 20);
+            this.IF_bandW_textBox.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "IF bandW";
+            // 
+            // Send_param_button
+            // 
+            this.Send_param_button.Location = new System.Drawing.Point(40, 246);
+            this.Send_param_button.Name = "Send_param_button";
+            this.Send_param_button.Size = new System.Drawing.Size(75, 23);
+            this.Send_param_button.TabIndex = 8;
+            this.Send_param_button.Text = "Send Param";
+            this.Send_param_button.UseVisualStyleBackColor = true;
+            this.Send_param_button.Click += new System.EventHandler(this.Send_param_button_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(201, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "MHz";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(201, 66);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "MHz";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(201, 136);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Hz";
+            // 
+            // Log_textBox
+            // 
+            this.Log_textBox.Location = new System.Drawing.Point(250, 28);
+            this.Log_textBox.Multiline = true;
+            this.Log_textBox.Name = "Log_textBox";
+            this.Log_textBox.Size = new System.Drawing.Size(403, 190);
+            this.Log_textBox.TabIndex = 12;
+            // 
+            // Avg_num_textBox
+            // 
+            this.Avg_num_textBox.Location = new System.Drawing.Point(96, 169);
+            this.Avg_num_textBox.Name = "Avg_num_textBox";
+            this.Avg_num_textBox.Size = new System.Drawing.Size(99, 20);
+            this.Avg_num_textBox.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(37, 171);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Avg Num";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Point_radioButton);
+            this.groupBox1.Controls.Add(this.Sweep_radioButton);
+            this.groupBox1.Location = new System.Drawing.Point(40, 195);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(180, 45);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Avg Mode";
+            // 
+            // Point_radioButton
+            // 
+            this.Point_radioButton.AutoSize = true;
+            this.Point_radioButton.Location = new System.Drawing.Point(97, 17);
+            this.Point_radioButton.Name = "Point_radioButton";
+            this.Point_radioButton.Size = new System.Drawing.Size(49, 17);
+            this.Point_radioButton.TabIndex = 1;
+            this.Point_radioButton.TabStop = true;
+            this.Point_radioButton.Text = "Point";
+            this.Point_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // Sweep_radioButton
+            // 
+            this.Sweep_radioButton.AutoSize = true;
+            this.Sweep_radioButton.Location = new System.Drawing.Point(17, 17);
+            this.Sweep_radioButton.Name = "Sweep_radioButton";
+            this.Sweep_radioButton.Size = new System.Drawing.Size(58, 17);
+            this.Sweep_radioButton.TabIndex = 0;
+            this.Sweep_radioButton.TabStop = true;
+            this.Sweep_radioButton.Text = "Sweep";
+            this.Sweep_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // Folder_name_textBox
+            // 
+            this.Folder_name_textBox.Location = new System.Drawing.Point(308, 234);
+            this.Folder_name_textBox.Name = "Folder_name_textBox";
+            this.Folder_name_textBox.Size = new System.Drawing.Size(345, 20);
+            this.Folder_name_textBox.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(249, 236);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(57, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Test name";
+            // 
+            // Measure_button
+            // 
+            this.Measure_button.Location = new System.Drawing.Point(578, 273);
+            this.Measure_button.Name = "Measure_button";
+            this.Measure_button.Size = new System.Drawing.Size(75, 23);
+            this.Measure_button.TabIndex = 19;
+            this.Measure_button.Text = "Measure";
+            this.Measure_button.UseVisualStyleBackColor = true;
+            this.Measure_button.Click += new System.EventHandler(this.Measure_button_Click);
+            // 
+            // Meas_time_textBox
+            // 
+            this.Meas_time_textBox.Location = new System.Drawing.Point(308, 260);
+            this.Meas_time_textBox.Name = "Meas_time_textBox";
+            this.Meas_time_textBox.Size = new System.Drawing.Size(46, 20);
+            this.Meas_time_textBox.TabIndex = 21;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(249, 262);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(55, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Meas time";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(356, 262);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(26, 13);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "Sec";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(865, 331);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.Meas_time_textBox);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.Measure_button);
+            this.Controls.Add(this.Folder_name_textBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Avg_num_textBox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.Log_textBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Send_param_button);
+            this.Controls.Add(this.IF_bandW_textBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Points_num_textBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.End_Freq_textBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Start_Freq_textBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Start_Freq_textBox;
+        private System.Windows.Forms.TextBox End_Freq_textBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Points_num_textBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox IF_bandW_textBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Send_param_button;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox Log_textBox;
+        private System.Windows.Forms.TextBox Avg_num_textBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton Point_radioButton;
+        private System.Windows.Forms.RadioButton Sweep_radioButton;
+        private System.Windows.Forms.TextBox Folder_name_textBox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button Measure_button;
+        private System.Windows.Forms.TextBox Meas_time_textBox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace FieldFox_1
+{
+    public partial class Form1 : Form
+    {
+        string hostName = "192.168.0.1";
+        TelnetConnection tc = new TelnetConnection();
+        string basicPath = @"C:\FieldFox";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tc.ReadTimeout = 10000; // 10 sec
+
+            Start_Freq_textBox.Text = "2";
+            End_Freq_textBox.Text = "600";
+            Points_num_textBox.Text = "201";
+            IF_bandW_textBox.Text = "10000";
+            Avg_num_textBox.Text = "1";
+            Point_radioButton.Checked = true;
+            Folder_name_textBox.Text = "";
+            Meas_time_textBox.Text = "20";
+        }
+
+        private void Send_param_button_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string avgMode = "SWEEP";
+
+                // open socket on hostName, which can be an IP address, or use host name (e.g. "A-N9912A-22762") used in lieu of IP address
+                tc.Open(hostName);
+                if (tc.IsOpen)
+                {
+
+                    Write("SYST:PRES;*OPC?");
+                    Write("*IDN?");
+                    Write("SENS:FREQ:START " + (Convert.ToInt32(Start_Freq_textBox.Text) * 1e6).ToString());
+                    Write("SENS:FREQ:STOP " + (Convert.ToInt32(End_Freq_textBox.Text) * 1e6).ToString());
+                    Write("SENS:SWEEP:POINTS " + (Convert.ToInt32(Points_num_textBox.Text)).ToString());
+                    Write("SENS:BWID " + (Convert.ToInt32(IF_bandW_textBox.Text)).ToString());
+                    Write("AVER:COUNt " + (Convert.ToInt32(Avg_num_textBox.Text)).ToString());
+                    if (Point_radioButton.Checked)
+                    {
+                        avgMode = "POINT";
+                    }
+                    Write("AVERage:MODE " + avgMode);
+                    Write("SYST:ERR?");
+
+                    tc.Dispose();
+
+                }
+                else
+                {
+                    Log_textBox.AppendText("Error opening " + hostName);
+
+                }
+                //FieldFox Programming Guide 5
+            }
+            catch (Exception exep)
+            {
+                Log_textBox.AppendText(exep.ToString() + "\n");
+
+            }
+
+        }
+
+
+        private void Measure_button_Click(object sender, EventArgs e)
+        {
+            if (Folder_name_textBox.Text.Length < 4)
+            {
+                System.Windows.Forms.MessageBox.Show("You have to enter valid test name!");
+            }
+            else
+            {
+                try
+                {
+
+                    tc.Open(hostName);
+                    if (tc.IsOpen)
+                    {
+                        //Create folder on NA
+                        string timeStamp = DateTime.Now.ToString("yyyyMMddHHmmssffff");
+                        string folderName = timeStamp + "_" + Folder_name_textBox.Text;
+
+                        Write("MMEMory:MDIRectory \"[INTERNAL]:\\" + folderName + "\"");
+
+                        //Create folder on PC
+                        string pathString = System.IO.Path.Combine(basicPath, folderName);
+                        Directory.CreateDirectory(pathString);
+
+                        //Move to folder
+                        Write("MMEM:CDIR \"[INTERNAL]:\\" + folderName + "\"");
+
+                        //Save files
+                        DateTime finalTime = DateTime.Now.AddSeconds(Convert.ToDouble(Meas_time_textBox.Text));
+                        int ind = 0;
+                        while (DateTime.Now<finalTime)
+                        {
+                            string fileName = DateTime.Now.ToString("HHmmssffff") + ind.ToString() +".s2p";
+                            Write("MMEMory:STORe:SNP \"" + fileName + "\"");
+                            ind++;
+                            Thread.Sleep(10); //sleep 10 ms
+                        }
+                        //Get files list
+                        string fileList = Write("MMEM:CAT?");
+                        List<string> names = fileList.Split(',').ToList<string>();
+
+                        //Copy file to PC
+                        foreach (string fileName in names)
+                        {
+                            if (fileName.Contains("s2p"))
+                            {
+                                string cleanFileName = fileName.Replace("\"", "").Replace("\\", "");
+                                string cmd = "MMEM:DATA? \"" + cleanFileName + "\"";
+                                var temp = Write(cmd,false);
+                                FileStream fs1 = new FileStream(pathString + "\\" + cleanFileName, FileMode.OpenOrCreate, FileAccess.Write);
+                                StreamWriter writer = new StreamWriter(fs1);
+                                writer.Write(temp);
+                                writer.Close();
+                            }
+                        }
+
+                        //Remove the folder on NA                        
+                        Write("MMEM:CDIR \"[INTERNAL]:\\\"");
+                        Write("MMEM:RDIR \"" + folderName + "\",\"recursive\"");
+                    }
+
+                }
+                catch (Exception exep)
+                {
+                    Log_textBox.AppendText(exep.ToString() + "\n");
+
+                }
+            }
+        }
+
+        public string Write(string s, bool writeToLog = true)
+        {
+            string ret = "";
+            Log_textBox.AppendText(s + "\n");
+            
+            tc.Write1(s);
+            if (s.IndexOf('?') >= 0)
+                ret = Read(writeToLog);
+            return ret;
+        }
+
+        public string Read(bool writeToLog = true)
+        {
+            string res = "";
+            try
+            {
+                res = tc.Read();
+                if (writeToLog)
+                {
+                    Log_textBox.AppendText(res + "\n");
+                }
+            }
+            catch (Exception exep)
+            {
+                Log_textBox.AppendText("Error during reading!\n");
+                Log_textBox.AppendText(exep.ToString() + "\n");
+            }
+            return res;
+        }
+
+    }
+
+    #region TelnetConnection - no need to edit
+
+    /// <summary>
+    /// Telnet Connection on port 5025 to an instrument
+    /// </summary>
+    public class TelnetConnection : IDisposable
+    {
+        TcpClient m_Client;
+        NetworkStream m_Stream;
+        bool m_IsOpen = false;
+        string m_Hostname;
+        int m_ReadTimeout = 1000; // ms
+        public delegate void ConnectionDelegate();
+        public event ConnectionDelegate Opened;
+        public event ConnectionDelegate Closed;
+        public bool IsOpen { get { return m_IsOpen; } }
+        public TelnetConnection() { }
+        public TelnetConnection(bool open) : this("localhost", true) { }
+        public TelnetConnection(string host, bool open)
+        {
+            if (open)
+                Open(host);
+        }
+        void CheckOpen()
+        {
+            if (!IsOpen)
+                throw new Exception("Connection not open.");
+        }
+        public string Hostname
+        {
+            get { return m_Hostname; }
+        }
+        public int ReadTimeout
+        {
+            set { m_ReadTimeout = value; if (IsOpen) m_Stream.ReadTimeout = value; }
+            get { return m_ReadTimeout; }
+        }
+        public void Write(string str)
+        {
+            //FieldFox Programming Guide 6
+            CheckOpen();
+            byte[] bytes = System.Text.ASCIIEncoding.ASCII.GetBytes(str);
+            m_Stream.Write(bytes, 0, bytes.Length);
+            m_Stream.Flush();
+        }
+        public void Write1(string str)
+        {
+            //FieldFox Programming Guide 6
+            str = str + "\n";
+            CheckOpen();
+            byte[] bytes = System.Text.ASCIIEncoding.ASCII.GetBytes(str);
+            m_Stream.Write(bytes, 0, bytes.Length);
+            m_Stream.Flush();
+        }
+        public void WriteLine(string str)
+        {
+            CheckOpen();
+            byte[] bytes = System.Text.ASCIIEncoding.ASCII.GetBytes(str);
+            m_Stream.Write(bytes, 0, bytes.Length);
+            WriteTerminator();
+        }
+        void WriteTerminator()
+        {
+            byte[] bytes = System.Text.ASCIIEncoding.ASCII.GetBytes("\r\n\0");
+            m_Stream.Write(bytes, 0, bytes.Length);
+            m_Stream.Flush();
+        }
+        public string Read()
+        {
+            CheckOpen();
+            return System.Text.ASCIIEncoding.ASCII.GetString(ReadBytes());
+        }
+
+        /// <summary>
+        /// Reads bytes from the socket and returns them as a byte[].
+        /// </summary>
+        /// <returns></returns>
+        public byte[] ReadBytes()
+        {
+            int i = m_Stream.ReadByte();
+            byte b = (byte)i;
+            int bytesToRead = 0;
+            var bytes = new List<byte>();
+            if ((char)b == '#')
+            {
+                bytesToRead = ReadLengthHeader();
+                if (bytesToRead > 0)
+                {
+                    i = m_Stream.ReadByte();
+                    if ((char)i != '\n') // discard carriage return after length header.
+                        bytes.Add((byte)i);
+                }
+            }
+            if (bytesToRead == 0)
+            {
+                while (i != -1 && b != (byte)'\n')
+                {
+                    bytes.Add(b);
+                    i = m_Stream.ReadByte();
+                    b = (byte)i;
+                }
+            }
+            else
+            {
+                int bytesRead = 0;
+                while (bytesRead < bytesToRead && i != -1)
+                {
+                    i = m_Stream.ReadByte();
+                    if (i != -1)
+                    {
+                        bytesRead++;
+                        // record all bytes except \n if it is the last char.
+                        if (bytesRead < bytesToRead || (char)i != '\n')
+                            bytes.Add((byte)i);
+                    }
+                }
+            }
+            return bytes.ToArray();
+        }
+
+        int ReadLengthHeader()
+        {
+            int numDigits = Convert.ToInt32(new string(new char[] { (char)m_Stream.ReadByte() }));
+            string bytes = "";
+            for (int i = 0; i < numDigits; ++i)
+                bytes = bytes + (char)m_Stream.ReadByte();
+
+            return Convert.ToInt32(bytes);
+        }
+
+
+        public void Open(string hostname)
+        {
+            if (IsOpen)
+                Close();
+            m_Hostname = hostname;
+            m_Client = new TcpClient(hostname, 5025);
+            m_Stream = m_Client.GetStream();
+            m_Stream.ReadTimeout = ReadTimeout;
+            m_IsOpen = true;
+            if (Opened != null)
+                Opened();
+        }
+        public void Close()
+        {
+            if (!m_IsOpen)
+                //FieldFox Programming Guide 7
+                return;
+            m_Stream.Close();
+            m_Client.Close();
+            m_IsOpen = false;
+            if (Closed != null)
+                Closed();
+        }
+        public void Dispose()
+        {
+            Close();
+        }
+    }
+    #endregion
+
+}
