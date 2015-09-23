bf9f01d883cb9b6189be3e28f9c118619bd71e6d
Moshe Oziel <uzikiko@gmail.com>
2015-09-23 23:40:21 +0300

    Complete matlab folderToObject function

--- Matlab/folderToObject.m
+++ Matlab/folderToObject.m
@@ -1,6 +1,7 @@
 function [ ret_obj ] = folderToObject( folder_name )
 %FOLDERTOOBJECT Summary of this function goes here
 %   Detailed explanation goes here
+    plotC = 1;
     ret_obj = 0;
     D = dir([folder_name,'\*.s2p']);
     S = sort({D.name});
@@ -10,14 +11,28 @@ function [ ret_obj ] = folderToObject( folder_name )
         ind = ind +1;
         times(ind) = FileNameToMili(file_name{1});
         data = read(rfdata.data,[folder_name,'\\',file_name{1}]);
-        freqs = data.freq;
+        freqs = data.freq/1e6;
         if ind==1
-            amp = zeros(length(S),length(data.freq));
-            phase = zeros(length(S),length(data.freq));
+            amp_db = zeros(length(S),length(data.freq));
+            phase_deg = amp_db;
         end
-        amp
+        % should be 21
+        amp_db(ind,1:2)=20*log10(abs(data.S_Parameters(2,1,:))); 
+        phase_deg(ind,1:2) = 180/pi*phase(data.S_Parameters(2,1,:));
+        
     end
-    figure; plot(times-times(1));
+    time_sec = (times-times(1))/1000;
+    if plotC
+        close all;
+        set(0, 'DefaulttextInterpreter', 'none');
+        amp_fig=figure; plot(time_sec,amp_db); title(folder_name); 
+        xlabel('Time [sec]'); ylabel('Amplitude [db]'); legend(cellstr(strcat(num2str(freqs),' MHz')));
+        phs_fig=figure; plot(time_sec,phase_deg); title(folder_name);
+        xlabel('Time [sec]'); ylabel('Phase [deg]'); legend(cellstr(strcat(num2str(freqs),' MHz')));
+        savefig(amp_fig,[folder_name,'\amp_fig']);
+        savefig(phs_fig,[folder_name,'\phs_fig']);
+        save([folder_name,'\data.mat'],'time_sec','amp_db','time_sec','phase_deg','freqs');
+    end
 
 end
 
